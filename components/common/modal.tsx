4f3a124ea169de624565db10ae2f7eac00aceddf
oms <po1202222@gmail.com>
2024-12-14 14:36:11 +0900

    [feat][오명섭] : Modal 기능 추가

--- components/common/modal.tsx
+++ components/common/modal.tsx
@@ -1,5 +1,17 @@
+'use client';
 import React from 'react';
+import MaterialModal from '@mui/material/Modal';
 
-export default function Modal() {
-  return <div>구현중입니다.</div>;
+interface ModalType {
+  open: boolean;
+  onClose?: () => void;
+  children?: React.ReactNode;
+}
+
+export default function Modal({open, onClose, children, ...props}: ModalType) {
+  return (
+    <MaterialModal open={open} onClose={onClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description" {...props}>
+      <>{children}</>
+    </MaterialModal>
+  );
 }
