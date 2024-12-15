78ee561652694f79879575d3c6549005a41d90c0
강시호 <noel0904@naver.com>
2024-12-15 23:10:08 +0900

    [feat][강시호] : 프로필 이미지 컴포넌트 추가

---
+++ components/common/profile-image.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Image from 'next/image';
+
+interface CircleImageProps {
+  src: string;
+  alt: string;
+  size?: number;
+  inset?: number; // 테두리가 안으로 들어간 정도
+}
+
+// size
+// 헤더에서 20, 45, 45
+// 프로필에서 60, 80, 164
+const ProfileImage: React.FC<CircleImageProps> = ({src, alt, size = 32, inset = 1}) => {
+  return (
+    <div className="relative" style={{width: size, height: size}}>
+      <Image src={src} alt={alt} width={size} height={size} className="w-full h-full object-cover rounded-full" />
+      <div className="absolute inset-0 rounded-full border border-[#CFDBEA]" style={{margin: inset}} />
+    </div>
+  );
+};
+
+export default ProfileImage;
