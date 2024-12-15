29dc008fdff6786be0e7797921ae8c59beaa3634
강시호 <noel0904@naver.com>
2024-12-15 23:58:36 +0900

    [refactor][강시호] : 함수 선언 수정

--- components/common/profile-image.tsx
+++ components/common/profile-image.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 
-interface CircleImageProps {
+interface ProfileImageProps {
   src: string;
   alt: string;
   size?: number;
@@ -11,11 +11,11 @@ interface CircleImageProps {
 // size
 // 헤더에서 20, 45, 45
 // 프로필에서 60, 80, 164
-export default function ProfileImage({src, alt, size = 32, inset = 1}: CircleImageProps) {
+export default function ProfileImage({src, alt, size = 32, inset = 1}: ProfileImageProps) {
   return (
     <div className="relative" style={{width: size, height: size}}>
       <Image src={src} alt={alt} width={size} height={size} className="w-full h-full object-cover rounded-full" />
       <div className="absolute inset-0 rounded-full border border-[#CFDBEA]" style={{margin: inset}} />
     </div>
   );
-}
+} 
