1329d3bec8eec4b4df4ffa90e10e7d479b6e5f37
강시호 <noel0904@naver.com>
2024-12-17 21:19:17 +0900

    [refactor][강시호] : rem단위 수정 및 color 수정

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -8,7 +8,7 @@ interface HeaderProps {
 
 export default function Header({children}: HeaderProps) {
   return (
-    <header className="w-[343px] h-[50px] lg:w-[704px] lg:h-[70px] xl:w-[1140px] xl:h-[70px] bg-[#101318] text-white px-[20px] lg:px-[60px] xl:px-[60px] flex justify-between items-center rounded-xl lg:rounded-2xl xl:rounded-2xl">
+    <header className="w-[21.438rem] h-[3.125rem] tablet:w-[44rem] tablet:h-[4.375rem] pc:w-[71.25rem] pc:h-[4.375rem] bg-black text-white px-[1.25rem] tablet:px-[3.75rem] pc:px-[3.75rem] flex justify-between items-center rounded-xl tablet:rounded-2xl pc:rounded-2xl">
       <Link href="/">
         <Image src="/logo.svg" alt="Logo" width={52} height={15} />
       </Link>
