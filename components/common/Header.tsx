331ebe256cc99cbfdb6ddfb6510f65e5764a392c
강시호 <noel0904@naver.com>
2024-12-19 19:07:54 +0900

    [refactor][강시호] : 파일명, 단위 수정

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -8,11 +8,11 @@ interface HeaderProps {
 
 export default function Header({children}: HeaderProps) {
   return (
-    <header className="w-[21.438rem] h-[3.125rem] tablet:w-[44rem] tablet:h-[4.375rem] pc:w-[71.25rem] pc:h-[4.375rem] bg-black text-white px-[1.25rem] tablet:px-[3.75rem] pc:px-[3.75rem] flex justify-between items-center rounded-xl tablet:rounded-2xl pc:rounded-2xl">
+    <header className="w-[21.438rem] h-[50pxr] tablet:w-[44rem] tablet:h-[70pxr] pc:w-[71.25rem] pc:h-[70pxr] bg-black text-white px-[20pxr] tablet:px-[60pxr] pc:px-[60pxr] flex justify-between items-center rounded-xl tablet:rounded-2xl pc:rounded-2xl">
       <Link href="/">
         <Image src="/logo.svg" alt="Logo" width={52} height={15} />
       </Link>
       <div>{children}</div>
     </header>
-  );
+  ); 
 }
