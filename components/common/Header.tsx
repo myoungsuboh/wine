e7f5005b4c001f02c0ecf29e62d55b8a083425af
Jeongbin1 <petitjb05@gamil.com>
2024-12-24 21:34:49 +0900

    [feat][한정빈] : 헤더에 로그인 상태 반영

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -8,11 +8,11 @@ interface HeaderProps {
 
 export default function Header({children}: HeaderProps) {
   return (
-    <header className="w-full h-[50pxr] tablet:h-[70pxr] pc:h-[70pxr] bg-black text-white px-[20pxr] tablet:px-[60pxr] pc:px-[60pxr] flex justify-between items-center rounded-xl tablet:rounded-2xl pc:rounded-2xl">
+    <header className="w-full h-50pxr tablet:h-70pxr pc:h-70pxr bg-black text-white px-20pxr tablet:px-60pxr pc:px-60pxr flex justify-between items-center rounded-xl tablet:rounded-2xl pc:rounded-2xl">
       <Link href="/">
         <Image src="/logo.svg" alt="Logo" width={52} height={15} />
       </Link>
       <div>{children}</div>
     </header>
-  ); 
+  );
 }
