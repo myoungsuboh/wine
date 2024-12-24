65dab0d7f5821d3cbe229cb5ce3a68272d6bf8d3
강시호 <noel0904@naver.com>
2024-12-24 17:26:05 +0900

    [refactor][강시호] : width값 수정

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -8,7 +8,7 @@ interface HeaderProps {
 
 export default function Header({children}: HeaderProps) {
   return (
-    <header className="w-[21.438rem] h-[50pxr] tablet:w-[44rem] tablet:h-[70pxr] pc:w-[71.25rem] pc:h-[70pxr] bg-black text-white px-[20pxr] tablet:px-[60pxr] pc:px-[60pxr] flex justify-between items-center rounded-xl tablet:rounded-2xl pc:rounded-2xl">
+    <header className="w-full h-[50pxr] tablet:h-[70pxr] pc:h-[70pxr] bg-black text-white px-[20pxr] tablet:px-[60pxr] pc:px-[60pxr] flex justify-between items-center rounded-xl tablet:rounded-2xl pc:rounded-2xl">
       <Link href="/">
         <Image src="/logo.svg" alt="Logo" width={52} height={15} />
       </Link>
