de55b69d66653589c271d0589ecaf06f13c9e1f8
Jeongbin1 <petitjb05@gamil.com>
2024-12-25 15:11:03 +0900

    [fix][한정빈] : 사라진 modal component 복구

---
+++ components/common/Modal.tsx
@@ -0,0 +1,15 @@
+'use client';
+import React from 'react';
+import MaterialModal from '@mui/material/Modal';
+interface ModalType {
+  open: boolean;
+  onClose?: () => void;
+  children?: React.ReactNode;
+}
+export default function Modal({open, onClose, children, ...props}: ModalType) {
+  return (
+    <MaterialModal open={open} onClose={onClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description" {...props}>
+      <>{children}</>
+    </MaterialModal>
+  );
+}
