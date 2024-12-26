e76ddc35a22ee9e2229f5acd88a33ea7b32a7cbd
Jeongbin1 <petitjb05@gamil.com>
2024-12-26 13:35:14 +0900

    [feat][서현우] : 랜딩 페이지 구현

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,3 +1,28 @@
+import Image from 'next/image';
+import bannerImage from '@/public/banner.png';
+import bannerImageMobile from '@/public/banner_m.png';
+import sectionImage1 from '@/public/section1.png';
+import sectionImage2 from '@/public/section2.png';
+import sectionImage3 from '@/public/section3.png';
+import sectionImage1Mobile from '@/public/section1_m.png';
+import sectionImage2Mobile from '@/public/section2_m.png';
+import sectionImage3Mobile from '@/public/seciton3_m.png';
+
 export default function Home() {
-  return <div>랜딩 페이지 입니다.</div>;
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="mt-6 xl:mt-20">
+        <Image src={bannerImageMobile} alt="banner" className="sm:hidden" />
+        <Image src={bannerImage} alt="banner" className="hidden sm:block" />
+      </div>
+      <div className="flex flex-col mt-12 justify-center items-center gap-12 sm:gap-24 sm:mt-20 xl:mt-40">
+        <Image src={sectionImage1Mobile} alt="section1" className="sm:hidden" />
+        <Image src={sectionImage1} alt="section1" className="hidden sm:block" />
+        <Image src={sectionImage2Mobile} alt="section2" className="sm:hidden" />
+        <Image src={sectionImage2} alt="section2" className="hidden sm:block" />
+        <Image src={sectionImage3Mobile} alt="section3" className="sm:hidden" />
+        <Image src={sectionImage3} alt="section3" className="hidden sm:block" />
+      </div>
+    </div>
+  );
 }
