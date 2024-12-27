dd7c9ead125f91c8e236b5c71e564b4d62bdd156
Jeongbin1 <petitjb05@gamil.com>
2024-12-27 16:22:18 +0900

    [feat][한정빈] : 공통 layout 반응형 설정

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -13,27 +13,27 @@ export default function MainLayout({children}: {children: React.ReactNode}) {
   const isLandingPage = pathname === '/';
 
   return (
-    <div>
+    <div className="p-16pxr tablet:pt-24pxr tablet:px-20pxr bg-gray-100">
       <Header>
         {isLandingPage ? (
           isLogin ? (
             <ProfileImage src="/default-profile.svg" />
           ) : (
-            <div className="auth-buttons flex gap-2">
+            <div className="flex gap-2">
               <Link href="/login">
-                <button className="login-btn px-4 py-2 text-white rounded">로그인</button>
+                <button className="px-4 py-2 ">로그인</button>
               </Link>
               <Link href="/signup">
-                <button className="signup-btn px-4 py-2 text-white rounded">회원가입</button>
+                <button className="px-4 py-2 ">회원가입</button>
               </Link>
             </div>
           )
         ) : isLogin ? (
           <ProfileImage src="/default-profile.svg" />
         ) : (
-          <div className="auth-buttons flex gap-2">
+          <div className="flex gap-2">
             <Link href="/login">
-              <button className="login-btn px-4 py-2 text-white rounded">로그인</button>
+              <button className="px-4 py-2 ">로그인</button>
             </Link>
           </div>
         )}
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -11,17 +11,17 @@ import sectionImage3Mobile from '@/public/seciton3_m.png';
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className="mt-6 xl:mt-20">
-        <Image src={bannerImageMobile} alt="banner" className="sm:hidden" />
-        <Image src={bannerImage} alt="banner" className="hidden sm:block" />
+      <div className="mt-6 pc:mt-20">
+        <Image src={bannerImageMobile} alt="banner" className="tablet:hidden" />
+        <Image src={bannerImage} alt="banner" className="hidden tablet:block" />
       </div>
-      <div className="flex flex-col mt-12 justify-center items-center gap-12 sm:gap-24 sm:mt-20 xl:mt-40">
-        <Image src={sectionImage1Mobile} alt="section1" className="sm:hidden" />
-        <Image src={sectionImage1} alt="section1" className="hidden sm:block" />
-        <Image src={sectionImage2Mobile} alt="section2" className="sm:hidden" />
-        <Image src={sectionImage2} alt="section2" className="hidden sm:block" />
-        <Image src={sectionImage3Mobile} alt="section3" className="sm:hidden" />
-        <Image src={sectionImage3} alt="section3" className="hidden sm:block" />
+      <div className="flex flex-col mt-12 justify-center items-center gap-12 tablet:gap-24 tablet:mt-20 pc:mt-40">
+        <Image src={sectionImage1Mobile} alt="section1" className="tablet:hidden" />
+        <Image src={sectionImage1} alt="section1" className="hidden tablet:block" />
+        <Image src={sectionImage2Mobile} alt="section2" className="tablet:hidden" />
+        <Image src={sectionImage2} alt="section2" className="hidden tablet:block" />
+        <Image src={sectionImage3Mobile} alt="section3" className="tablet:hidden" />
+        <Image src={sectionImage3} alt="section3" className="hidden tablet:block" />
       </div>
     </div>
   );
