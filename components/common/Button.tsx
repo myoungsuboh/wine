c6e32c6de18e07d7b9af2b91c3d58ad87f83af56
Jeongbin1 <petitjb05@gamil.com>
2024-12-27 16:38:20 +0900

    [feat][한정빈] : 버튼 수정

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,21 +1,17 @@
 'use client';
-import MaterialButton from '@mui/material/Button';
 import React from 'react';
 
 interface ModalType {
   className?: string;
-  style?: object;
-  variant?: 'text' | 'contained' | 'outlined';
-  size?: 'small' | 'medium' | 'large';
   type?: 'button' | 'submit' | 'reset';
   onClick?: () => void;
   children?: React.ReactNode;
 }
 
-export default function Button({className, style, variant, size, type = 'button', children, onClick, ...props}: ModalType) {
+export default function Button({className = '', type = 'button', children, onClick}: ModalType) {
   return (
-    <MaterialButton className={className} sx={style} variant={variant || 'contained'} size={size || 'small'} type={type} onClick={onClick} {...props}>
+    <button className={className} type={type} onClick={onClick}>
       {children}
-    </MaterialButton>
+    </button>
   );
 }
